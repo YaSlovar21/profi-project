cb3c14063e847499738e20fa3ecf4840a893dc6f
YaSlovar_work <stanko.tsep@yandex.ru>
2021-05-21 16:18:54 +0700

    profi_sborka_startJS

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,31 @@
-import Swiper from 'swiper';
-
+import Swiper from 'swiper/bundle';
+import WOW from 'wow.js';
 
 import 'swiper/swiper-bundle.css';
-import '../pages/index.css';
-import '../styles/animate.css';
+import 'animate.css';
 
-//import 'wow.js';
+
+import '../pages/index.css';
+
+const wowAnimation = new WOW({
+  boxClass:     'wow',      // animated element css class (default is wow)
+  animateClass: 'animate__animated', // animation css class (default is animated)
+  offset:       0,          // distance to the element when triggering the animation (default is 0)
+  mobile:       true,       // trigger animations on mobile devices (default is true)
+  live:         true,       // act on asynchronously loaded content (default is true)
+  scrollContainer: null,    // optional scroll container selector, otherwise use window,
+  resetAnimation: true,     // reset animation on end (default is true)
+});
+wowAnimation.init();
+
 
 const commercOffersSlider  = new Swiper('.khan__book-container', {
         direction: 'horizontal',
-        effect: 'coverflow',
+        effect: 'slide',
         speed: 700,
         preloadImages: false,
-        lazy: true,
-        zoom: true,
+        //lazy: true,
+        //zoom: true,
         cssMode: false,
         mousewheel: false,
 
@@ -45,7 +57,22 @@ const commercOffersSlider  = new Swiper('.khan__book-container', {
         },
 });
 
-//const wowAnimation = new WOW();
-//wowAnimation.init();
+commercOffersSlider.init();
 
+const popupImageSelector = '.popup-viewport';
+const projectToPopupSelector = '.projects-item';
+/*
+Array.from(document.querySelectorAll(projectToPopupSelector))
+  .forEach(item => {
+    let itemImg
+    item.addEventListener('click')
+  })
+*/
+
+const popupProject = new PopupWithImage({
+    imageInPopupSelector: '', 
+    imageDescInPopupSelector :'',
+  },
+  popupImageSelector
+);
 
